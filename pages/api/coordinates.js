5fbb9f19edf62b0def75f23b5f2c7bfcdc31c1c6
Benny <benny.ng@usp.br>
2022-10-17 15:10:31 -0300

    coordinates API

---
+++ pages/api/coordinates.js
@@ -0,0 +1,13 @@
+import { coordinates } from "../../data/coordinates";
+
+function control(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(coordinates);
+  } else if (req.method === "POST") {
+    const coordinate = req.body.coordinate;
+    coordinates.push(coordinate);
+    res.status(201).json(coordinate);
+  }
+}
+
+export default control;
