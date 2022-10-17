0167f978e2c1caa3c717cf042b0369376f488a03
Benny <benny.ng@usp.br>
2022-10-17 15:28:00 -0300

    ajuste api

--- pages/api/coordinates.js
+++ pages/api/coordinates.js
@@ -5,7 +5,8 @@ function control(req, res) {
     res.status(200).json(coordinates);
   } else if (req.method === "POST") {
     const coordinate = req.body;
-    res.status(201).json(coordinate);
+    coordinates.key = coordinate;
+    res.status(201).json(move);
   }
 }
 
