c17afda0c6e96ac06c6c0201300a88aed6549607
Benny <benny.ng@usp.br>
2022-10-28 11:54:22 -0300

    Revert "teste API ESP"
    This reverts commit 7fcaaa64a7a586619264fa426c94cf309f0ad26c.

--- pages/api/coordinates.js
+++ pages/api/coordinates.js
@@ -4,8 +4,7 @@ function coordinates(req, res) {
   if (req.method === "GET") {
     res.status(200).json(coords);
   } else if (req.method === "POST") {
-    // const latitude = req.body.latitude;
-    const latitude = req.body;
+    const latitude = req.body.latitude;
     const longitude = req.body.longitude;
     const status = req.body.status;
     coords.latitude = latitude;
