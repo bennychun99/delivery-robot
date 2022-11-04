5159f1c7136800e7ec77f02339f80e4b1eff1a2d
Benny <benny.ng@usp.br>
2022-11-04 15:19:18 -0300

    altera post

--- pages/api/coordinates.js
+++ pages/api/coordinates.js
@@ -4,7 +4,7 @@ function coordinates(req, res) {
   if (req.method === "GET") {
     res.status(200).json(coords);
   } else if (req.method === "POST") {
-    const latitude = req.body.latitude;
+    const latitude = req.body;
     const longitude = req.body.longitude;
     const status = req.body.status;
     coords.latitude = latitude;
