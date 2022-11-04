08f4686506f281eadd108fcdbd18b369347865d3
Benny <benny.ng@usp.br>
2022-11-04 15:26:10 -0300

    ajuste api coords

--- data/coordinates.js
+++ data/coordinates.js
@@ -1,1 +1,8 @@
-export const coords = { latitude: 0, longitude: 0, status: 0 };
+export const coords = {
+  latitude: 0,
+  longitude: 0,
+  status: 0,
+  magnetometerX: 0,
+  magnetometerY: 0,
+  magnetometerZ: 0,
+};
--- pages/api/coordinates.js
+++ pages/api/coordinates.js
@@ -4,12 +4,18 @@ function coordinates(req, res) {
   if (req.method === "GET") {
     res.status(200).json(coords);
   } else if (req.method === "POST") {
-    const latitude = req.body;
-    const longitude = req.body.longitude;
+    const latitude = req.body.locationLatitude;
+    const longitude = req.body.locationLongitude;
+    const magnetometerX = req.body.magnetometerX;
+    const magnetometerY = req.body.magnetometerY;
+    const magnetometerZ = req.body.magnetometerZ;
     const status = req.body.status;
     coords.latitude = latitude;
     coords.longitude = longitude;
     coords.status = status;
+    coords.magnetometerX = magnetometerX;
+    coords.magnetometerY = magnetometerY;
+    coords.magnetometerZ = magnetometerZ;
     res.status(201).json(req.body);
   }
 }
