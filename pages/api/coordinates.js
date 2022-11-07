4c03c457688d430e7a56e451d5d1d757ec39ae91
Benny <benny.ng@usp.br>
2022-11-07 20:14:44 -0300

    ajusteApi

--- pages/api/coordinates.js
+++ pages/api/coordinates.js
@@ -4,7 +4,7 @@ function coordinates(req, res) {
   if (req.method === "GET") {
     res.status(200).json(coords);
   } else if (req.method === "POST") {
-    const latitude = req.body;
+    const latitude = req.body.locationLatitude;
     const longitude = req.body.locationLongitude;
     const logginTime = req.body.logginTime;
     const motionYaw = req.body.motionYaw;
